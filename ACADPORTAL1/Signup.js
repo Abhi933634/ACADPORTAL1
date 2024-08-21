5b9f5dfee84a483456c41e44d76e65b5ab8211ad
Ashutosh kumar gupta <ashucomp87@gmail.com>
2024-08-21 21:23:07 +0530

    new

---
+++ ACADPORTAL1/Signup.js
@@ -0,0 +1,54 @@
+﻿function validate() {
+
+	if (document.myForm.First_Name.value == "") {
+
+		alert("Please provide First Name !");
+		document.myForm.First_Name.focus();
+		return false;
+	}
+
+	if (document.myForm.Last_Name.value == "") {
+
+		alert("Please provide Last Name !");
+		document.myForm.Last_Name.focus();
+		return false;
+	}
+
+	if (document.myForm.Mobile.value == "") {
+
+		alert("Please provide mobile number !!");
+		document.myForm.Mobile.focus();
+		return false;
+	}
+
+	if (document.myForm.Email.value == "") {
+
+		alert("Please provide valid Email !!");
+		document.myForm.Email.focus();
+		return false;
+	}
+
+	if (document.myForm.Password.value == "") {
+
+		alert("Please Enter Password !!");
+		document.myForm.Password.focus();
+		return false;
+	}
+
+	if (document.myForm.city.value == "") {
+
+		alert("Please provide city");
+		document.myForm.city.focus();
+		return false;
+	}
+
+	if (document.myForm.state.value == "") {
+
+		alert("Please Enter State !!");
+		document.myForm.state.focus();
+		return false;
+	}
+
+	return (true);
+
+}
